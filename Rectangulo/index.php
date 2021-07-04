6c6771d4670f42404ac8d305ee4f667bb7005a58
Bryan Bueno Gomez <Bryan Bueno Gomez@LAPTOP-9NJ8T4FD>
2021-07-04 08:23:55 -0500

    Proyecto clase Programacion

--- Rectangulo/index.php
+++ Rectangulo/index.php
@@ -1,3 +1,10 @@
 <?php
 
-include 
+include 'includes/class.rectangulo';
+$rectangulo = new CRectangulo ();
+echo $rectangulo->get_formulario();
+
+if (isset($_POST['guardar'])) {
+    echo"El resultado del area es igual a <strong>" . $rectangulo->area($_POST['largo'], $_POST['ancho']) 
+      . " Elresultado del perimetro es igual a <strong>". $rectangulo->perimetro($_POST['largo'], $_POST['ancho']) ;
+}
