4a1fe937e347e04420805a3843ebaae600236a70
Bryan Bueno Gomez <Bryan Bueno Gomez@LAPTOP-9NJ8T4FD>
2021-07-14 13:24:00 -0500

    Proyecto clase Programacion

--- Bucles/fibonacci.php
+++ Bucles/fibonacci.php
@@ -1,18 +1,17 @@
 <?php
  
-function fibonacci($n)
-{
-    $fibonacci  = [0,1];
- 
-  for($i=2;$i<=$n;$i++)
-    {
-        $fibonacci[] = $fibonacci[$i-1]+$fibonacci[$i-2];
-    }
-    echo $fibonacci[$n];
-}
- 
-fibonacci(10);
- 
+$num1= 0;
+$num2= 1;
+$num3;
+ for ($i= 0 ; $i<= 20;$i++){
+     $num3= $num1 + $num2;
+     $num1 = $num2;
+     $num2 = $num3;
+     echo $num3 . "<br>";
+     echo " ";
+ }
+
+
 ?>
 
 
