ffc0c797193f8d822ca6120d4c85e29cd1960278
Bryan Bueno Gomez <Bryan Bueno Gomez@LAPTOP-9NJ8T4FD>
2021-09-11 08:14:09 -0500

    Proyecto clase Programacion

---
+++ login/partials/header.php
@@ -0,0 +1,4 @@
+<header>
+  <a href="/php-login">Tarea de Login</a>
+</header>
+    
---
+++ login/signup.php
@@ -0,0 +1,48 @@
+<?php
+
+  require 'database.php';
+
+  $message = '';
+
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+      $message = 'Su cuenta fue creada con éxito';
+    } else {
+      $message = "Lo siento,se produjo un error al crear su cuenta";
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Registrate</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+
+    <?php require 'partials/header.php' ?>
+
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+
+    <h1>Registrate</h1>
+    <span>or <a href="login.php">Login</a></span>
+
+    <form action="signup.php" method="POST">
+      <input name="email" type="text" placeholder="Ingrese un correo electronico">
+      <input name="password" type="password" placeholder="Ingrese una contraseña">
+      <input name="confirm_password" type="password" placeholder="confirme su contraseña">
+      <input type="submit" value="Registar">
+    </form>
+
+  </body>
+</html>
